9b9abf908fafa47fa93307c8328a0c30346d2b02
harin1124 <lolrin1124@gmail.com>
2022-07-21 22:26:34 +0900

    tobi-spring study run

---
+++ src/main/java/com/example/tobispring/TobiSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.tobispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class TobiSpringApplication {
+	public static void main(String[] args) {
+		//SpringApplication.run(TobiSpringApplication.class, args);
+		System.out.println("tobi spring study run");
+	}
+}
