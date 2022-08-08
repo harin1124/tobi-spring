090e5161f9a0442b5d3afa808447f01e8cb44c04
movhr <lolrin1124@gmail.com>
2022-08-08 18:56:36 +0900

    스프링의 애플리케이션 컨텍스트 사용

--- src/main/java/com/example/tobispring/user/dao/DaoFactory.java
+++ src/main/java/com/example/tobispring/user/dao/DaoFactory.java
@@ -1,18 +1,23 @@
 package com.example.tobispring.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// @Configuration 스프링이 빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스라고 인식하도록 하는 어노테이션
+@Configuration
 public class DaoFactory {
+	/*
+		스프링 프레임워크의 빈 팩토리 or 애플리케이션 컨텍스트가 IoC 방식의 기능 제공 시, 사용할 설정정보
+		XML과 같은 스프링 전용 설정정보라고 보아도 됌
+	*/
+
+	// @Bean 오브젝트를 만들어주는 메소드
+	@Bean
 	public UserDao userDao(){
 		return new UserDao(connectionMaker());
 	}
 
-	public AccountDao accountDao(){
-		return new AccountDao(connectionMaker());
-	}
-
-	public MessageDao messageDao(){
-		return new MessageDao(connectionMaker());
-	}
-
+	@Bean
 	public ConnectionMaker connectionMaker(){
 		return new DConnectionMaker();
 	}
