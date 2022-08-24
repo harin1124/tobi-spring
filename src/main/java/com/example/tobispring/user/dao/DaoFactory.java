37670aee0e5e4334c3623bd4688a489ab6481ddd
harin1124 <lolrin1124@gmail.com>
2022-08-24 23:46:46 +0900

    xml 파일을 이용한 dataSource 사용

--- src/main/java/com/example/tobispring/user/dao/DaoFactory.java
+++ src/main/java/com/example/tobispring/user/dao/DaoFactory.java
@@ -2,23 +2,28 @@ package com.example.tobispring.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
-// @Configuration 스프링이 빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스라고 인식하도록 하는 어노테이션
+import javax.sql.DataSource;
+
 @Configuration
 public class DaoFactory {
-	/*
-		스프링 프레임워크의 빈 팩토리 or 애플리케이션 컨텍스트가 IoC 방식의 기능 제공 시, 사용할 설정정보
-		XML과 같은 스프링 전용 설정정보라고 보아도 됌
-	*/
-
-	// @Bean 오브젝트를 만들어주는 메소드
 	@Bean
 	public UserDao userDao(){
-		return new UserDao(connectionMaker());
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
+		return userDao;
 	}
 
 	@Bean
-	public ConnectionMaker connectionMaker(){
-		return new DConnectionMaker();
+	public DataSource dataSource(){
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mariadb://localhost:3306/DB명");
+		dataSource.setUsername("유저명");
+		dataSource.setPassword("비밀번호");
+
+		return dataSource;
 	}
 }
